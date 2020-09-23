6c850a973260325c714455a0cab9608100dcdffa
asd0755 <asd0755@naver.com>
2020-09-23 11:21:45 +0900

    DB Connection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const express = require('express');
 const app = express();
 
@@ -14,3 +15,32 @@ app.listen(PORT, () => {
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + "/index.html");
 });
+
+// mysql
+const mysql= require('mysql');
+const connection = mysql.createConnection({
+  host     : '192.168.1.70',
+  port : 5507,
+  user     : 'ejkim',
+  password : 'ejkim',
+  database : 'my_todo'
+});
+connection.connect();
+
+connection.query('select task from todo_table where id=1', function(err, result) {
+  console.log(result);
+  console.log(JSON.stringify(result)); 
+  // console.log(err);
+})
+
+
+
+// connection.query(`select * from todo_table where id = :id`, function(err, result) {
+//   const id = param.id;
+
+//   query.mapping('id', param.id)
+//   console.log("test");
+//   console.log(result); 
+//   console.log(err);
+// })
+
